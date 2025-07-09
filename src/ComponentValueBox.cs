2fda976975da9441ec1ef445fab24ad1a3ac2a2a
Bas Geertsema <mail@basgeertsema.net>
2025-07-09 09:25:06 +0200

    ComponentValueBox

---
+++ src/ComponentValueBox.cs
@@ -0,0 +1,295 @@
+using System;
+
+namespace Wasmtime
+{
+    /// <summary>
+    /// Represents a record entry in a WebAssembly component.
+    /// </summary>
+    public struct ComponentRecordEntry
+    {
+        /// <summary>
+        /// The name of the record field.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The value of the record field.
+        /// </summary>
+        public object? Value { get; set; }
+    }
+
+    /// <summary>
+    /// Allocation free container for a single component value
+    /// </summary>
+    public readonly struct ComponentValueBox
+    {
+        internal readonly ComponentValueKind Kind;
+        internal readonly ComponentValueUnion Union;
+        internal readonly object? ObjectValue;
+
+        internal ComponentValueBox(ComponentValueKind kind, ComponentValueUnion of)
+        {
+            Kind = kind;
+            Union = of;
+            ObjectValue = null;
+        }
+
+        internal ComponentValueBox(ComponentValueKind kind, object? objectValue)
+        {
+            Kind = kind;
+            Union = default;
+            ObjectValue = objectValue;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="bool"/> value.
+        /// </summary>
+        public bool AsBool()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.Bool);
+            return Union.boolean;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="sbyte"/> value.
+        /// </summary>
+        public sbyte AsS8()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.S8);
+            return Union.s8;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="byte"/> value.
+        /// </summary>
+        public byte AsU8()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.U8);
+            return Union.u8;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="short"/> value.
+        /// </summary>
+        public short AsS16()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.S16);
+            return Union.s16;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="ushort"/> value.
+        /// </summary>
+        public ushort AsU16()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.U16);
+            return Union.u16;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="int"/> value.
+        /// </summary>
+        public int AsS32()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.S32);
+            return Union.s32;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="uint"/> value.
+        /// </summary>
+        public uint AsU32()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.U32);
+            return Union.u32;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="long"/> value.
+        /// </summary>
+        public long AsS64()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.S64);
+            return Union.s64;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="ulong"/> value.
+        /// </summary>
+        public ulong AsU64()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.U64);
+            return Union.u64;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="float"/> value.
+        /// </summary>
+        public float AsF32()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.F32);
+            return Union.f32;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="double"/> value.
+        /// </summary>
+        public double AsF64()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.F64);
+            return Union.f64;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="char"/> value.
+        /// </summary>
+        public char AsChar()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.Char);
+            return (char)Union.character;
+        }
+
+        /// <summary>
+        /// "Unbox" a <see cref="string"/> value.
+        /// </summary>
+        public string AsString()
+        {
+            ThrowIfNotOfCorrectKind(ComponentValueKind.String);
+            return (string)(ObjectValue ?? string.Empty);
+        }
+
+        /// <summary>
+        /// "Unbox" as a generic type.
+        /// </summary>
+        public T? As<T>() where T : class
+        {
+            return (T?)ObjectValue;
+        }
+
+        private void ThrowIfNotOfCorrectKind(ComponentValueKind expectedKind)
+        {
+            if (Kind != expectedKind)
+            {
+                throw new InvalidCastException($"Cannot convert from `{Kind}` to `{expectedKind}`");
+            }
+        }
+
+        /// <summary>
+        /// "Box" a bool without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(bool value)
+        {
+            return new ComponentValueBox(ComponentValueKind.Bool, new ComponentValueUnion { boolean = value });
+        }
+
+        /// <summary>
+        /// "Box" a sbyte without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(sbyte value)
+        {
+            return new ComponentValueBox(ComponentValueKind.S8, new ComponentValueUnion { s8 = value });
+        }
+
+        /// <summary>
+        /// "Box" a byte without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(byte value)
+        {
+            return new ComponentValueBox(ComponentValueKind.U8, new ComponentValueUnion { u8 = value });
+        }
+
+        /// <summary>
+        /// "Box" a short without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(short value)
+        {
+            return new ComponentValueBox(ComponentValueKind.S16, new ComponentValueUnion { s16 = value });
+        }
+
+        /// <summary>
+        /// "Box" a ushort without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(ushort value)
+        {
+            return new ComponentValueBox(ComponentValueKind.U16, new ComponentValueUnion { u16 = value });
+        }
+
+        /// <summary>
+        /// "Box" an int without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(int value)
+        {
+            return new ComponentValueBox(ComponentValueKind.S32, new ComponentValueUnion { s32 = value });
+        }
+
+        /// <summary>
+        /// "Box" a uint without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(uint value)
+        {
+            return new ComponentValueBox(ComponentValueKind.U32, new ComponentValueUnion { u32 = value });
+        }
+
+        /// <summary>
+        /// "Box" a long without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(long value)
+        {
+            return new ComponentValueBox(ComponentValueKind.S64, new ComponentValueUnion { s64 = value });
+        }
+
+        /// <summary>
+        /// "Box" a ulong without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(ulong value)
+        {
+            return new ComponentValueBox(ComponentValueKind.U64, new ComponentValueUnion { u64 = value });
+        }
+
+        /// <summary>
+        /// "Box" a float without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(float value)
+        {
+            return new ComponentValueBox(ComponentValueKind.F32, new ComponentValueUnion { f32 = value });
+        }
+
+        /// <summary>
+        /// "Box" a double without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(double value)
+        {
+            return new ComponentValueBox(ComponentValueKind.F64, new ComponentValueUnion { f64 = value });
+        }
+
+        /// <summary>
+        /// "Box" a char without any heap allocations
+        /// </summary>
+        public static implicit operator ComponentValueBox(char value)
+        {
+            return new ComponentValueBox(ComponentValueKind.Char, new ComponentValueUnion { character = (uint)value });
+        }
+
+        /// <summary>
+        /// "Box" a string
+        /// </summary>
+        public static implicit operator ComponentValueBox(string value)
+        {
+            return new ComponentValueBox(ComponentValueKind.String, value);
+        }
+
+        /// <summary>
+        /// "Box" an arbitrary reference type
+        /// </summary>
+        public static ComponentValueBox AsBox<T>(T? value) where T : class
+        {
+            if (value is string str)
+            {
+                return new ComponentValueBox(ComponentValueKind.String, str);
+            }
+
+            // For now, treat other reference types as records
+            return new ComponentValueBox(ComponentValueKind.Record, value);
+        }
+    }
+}
---
+++ src/ComponentValueHelpers.cs
@@ -0,0 +1,185 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Wasmtime
+{
+    /// <summary>
+    /// Helper methods for converting between ComponentValueBox and native ComponentValue
+    /// </summary>
+    internal static class ComponentValueHelpers
+    {
+        /// <summary>
+        /// Convert a ComponentValueBox to a native ComponentValue
+        /// </summary>
+        public static unsafe ComponentValue FromValueBox(Store store, ComponentValueBox box)
+        {
+            var value = new ComponentValue
+            {
+                kind = box.Kind,
+                of = new ComponentValueUnion()
+            };
+
+            switch (box.Kind)
+            {
+                case ComponentValueKind.Bool:
+                    value.of.boolean = box.Union.boolean;
+                    break;
+
+                case ComponentValueKind.S8:
+                    value.of.s8 = box.Union.s8;
+                    break;
+
+                case ComponentValueKind.U8:
+                    value.of.u8 = box.Union.u8;
+                    break;
+
+                case ComponentValueKind.S16:
+                    value.of.s16 = box.Union.s16;
+                    break;
+
+                case ComponentValueKind.U16:
+                    value.of.u16 = box.Union.u16;
+                    break;
+
+                case ComponentValueKind.S32:
+                    value.of.s32 = box.Union.s32;
+                    break;
+
+                case ComponentValueKind.U32:
+                    value.of.u32 = box.Union.u32;
+                    break;
+
+                case ComponentValueKind.S64:
+                    value.of.s64 = box.Union.s64;
+                    break;
+
+                case ComponentValueKind.U64:
+                    value.of.u64 = box.Union.u64;
+                    break;
+
+                case ComponentValueKind.F32:
+                    value.of.f32 = box.Union.f32;
+                    break;
+
+                case ComponentValueKind.F64:
+                    value.of.f64 = box.Union.f64;
+                    break;
+
+                case ComponentValueKind.Char:
+                    value.of.character = box.Union.character;
+                    break;
+
+                case ComponentValueKind.String:
+                    if (box.ObjectValue is string str)
+                    {
+                        var bytes = Encoding.UTF8.GetBytes(str);
+                        var ptr = Marshal.AllocHGlobal(bytes.Length + 1);
+                        Marshal.Copy(bytes, 0, ptr, bytes.Length);
+                        Marshal.WriteByte(ptr, bytes.Length, 0);
+                        value.of.@string = (byte*)ptr;
+                    }
+                    break;
+
+                // TODO: Implement complex types (List, Record, Tuple, Variant, Enum, Option, Result, Flags)
+                default:
+                    throw new NotImplementedException($"Component value kind {box.Kind} is not yet implemented");
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Convert a native ComponentValue to a ComponentValueBox
+        /// </summary>
+        public static unsafe ComponentValueBox ToValueBox(Store store, ComponentValue value)
+        {
+            switch (value.kind)
+            {
+                case ComponentValueKind.Bool:
+                    return value.of.boolean;
+
+                case ComponentValueKind.S8:
+                    return value.of.s8;
+
+                case ComponentValueKind.U8:
+                    return value.of.u8;
+
+                case ComponentValueKind.S16:
+                    return value.of.s16;
+
+                case ComponentValueKind.U16:
+                    return value.of.u16;
+
+                case ComponentValueKind.S32:
+                    return value.of.s32;
+
+                case ComponentValueKind.U32:
+                    return value.of.u32;
+
+                case ComponentValueKind.S64:
+                    return value.of.s64;
+
+                case ComponentValueKind.U64:
+                    return value.of.u64;
+
+                case ComponentValueKind.F32:
+                    return value.of.f32;
+
+                case ComponentValueKind.F64:
+                    return value.of.f64;
+
+                case ComponentValueKind.Char:
+                    return (char)value.of.character;
+
+                case ComponentValueKind.String:
+                    if (value.of.@string != null)
+                    {
+                        // Count bytes until null terminator
+                        int length = 0;
+                        byte* ptr = value.of.@string;
+                        while (ptr[length] != 0) length++;
+                        
+                        // Convert to string
+                        byte[] bytes = new byte[length];
+                        Marshal.Copy((IntPtr)value.of.@string, bytes, 0, length);
+                        return Encoding.UTF8.GetString(bytes);
+                    }
+                    return string.Empty;
+
+                // TODO: Implement complex types
+                default:
+                    throw new NotImplementedException($"Component value kind {value.kind} is not yet implemented");
+            }
+        }
+
+        /// <summary>
+        /// Free a native ComponentValue
+        /// </summary>
+        public static unsafe void ReleaseValue(ComponentValue* value)
+        {
+            if (value == null) return;
+
+            // Free allocated strings
+            if (value->kind == ComponentValueKind.String && value->of.@string != null)
+            {
+                Marshal.FreeHGlobal((IntPtr)value->of.@string);
+                value->of.@string = null;
+            }
+
+            // TODO: Free complex types (lists, records, etc.)
+        }
+
+        /// <summary>
+        /// Native interop methods
+        /// </summary>
+        internal static class Native
+        {
+            [DllImport(Engine.LibraryName)]
+            public static extern unsafe ComponentValue* wasmtime_component_val_new();
+
+            [DllImport(Engine.LibraryName)]
+            public static extern unsafe void wasmtime_component_val_delete(ComponentValue* value);
+        }
+    }
+}
